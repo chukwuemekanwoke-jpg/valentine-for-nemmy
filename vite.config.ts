afa396f1cae927b4f6cca7273182c8f6ec5f6d71
chukwuemekanwoke-jpg <chukwuemeka.nwoke@ucdconnect.ie>
2026-02-01 13:33:51 +0000

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,9 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   base: './',
+  define: {
+    'process.env.API_KEY': JSON.stringify(process.env.API_KEY)
+  },
   server: {
     port: 5173,
     open: true
